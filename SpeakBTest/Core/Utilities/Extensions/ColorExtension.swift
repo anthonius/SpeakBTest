551af09408c29fe3545f853de3f1d330d41bccc7
Anthonius <anthonius@gdn-commerce.com>
2025-03-09 19:08:26 +0700

    adding background and color extension

---
+++ SpeakBTest/Core/Utilities/Extensions/ColorExtension.swift
@@ -0,0 +1,13 @@
+//
+//  ColorExtension.swift
+//  SpeakBTest
+//
+//  Created by Anthonius on 09/03/25.
+//
+
+import SwiftUI
+
+extension Color {
+    static let primaryBlue = Color(red: 88 / 255, green: 167 / 255, blue: 248 / 255)
+    static let purpleBg = Color(red: 218 / 255, green: 218 / 255, blue: 252 / 255)
+}
--- SpeakBTest/Features/Signup/Views/SignupView.swift
+++ SpeakBTest/Features/Signup/Views/SignupView.swift
@@ -9,40 +9,52 @@ import SwiftUI
 
 struct SignupView: View {
     var body: some View {
-        VStack {
-            HStack {
-                Spacer()
-                Button(action: {
-                    print("Button tapped!")
-                }) {
-                    Image(systemName: "xmark")
-                        .padding(12)
-                        .background(.white)
-                        .clipShape(Circle())
-                        .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 5)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.white, Color.purpleBg]),
+                           startPoint: .bottom,
+                           endPoint: .top)
+            .ignoresSafeArea()
+            ScrollView {
+                VStack {
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            print("Button tapped!")
+                        }) {
+                            Image(systemName: "xmark")
+                                .font(.system(size: 20, weight: .bold))
+                                .foregroundColor(.gray)
+                                .padding(12)
+                                .background(.white)
+                                .clipShape(Circle())
+                                .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 5)
+                        }
+                    }
+                    .padding(.trailing, 20)
+                    
+                    Text("Hello")
+                        .font(.system(size: 36, weight: .bold))
+                    Text("SpeakBUDDY")
+                        .font(.system(size: 36, weight: .bold))
+                    
+                    Image("robot")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 187, height: 160)
+                    
+                    Text("スピークバディで")
+                    Text("レベルアップ")
+                    
+                    Button(action: {
+                        print("Button tapped!")
+                    }) {
+                        Text("プランに登録する")
+                            .foregroundColor(.white)
+                            .frame(width: 350, height: 50)
+                            .background(Color.blue)
+                            .clipShape(RoundedRectangle(cornerRadius: 25))
+                    }
                 }
-            }
-            .padding(.trailing, 20)
-            
-            Text("Hello")
-            Text("SpeakBUDDY")
-            
-            Image("robot")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 187, height: 160)
-            
-            Text("スピークバディで")
-            Text("レベルアップ")
-            
-            Button(action: {
-                print("Button tapped!")
-            }) {
-                Text("プランに登録する")
-                    .foregroundColor(.white)
-                    .frame(width: 350, height: 50)
-                    .background(Color.blue)
-                    .clipShape(RoundedRectangle(cornerRadius: 25))
             }
         }
     }
