4ccfba56a7ae929aef8f7d3ea755e0d593a291b7
Anthonius <anthonius@gdn-commerce.com>
2025-03-09 19:46:41 +0700

    Extracting view to increase readability

--- SpeakBTest/Features/Signup/Views/SignupView.swift
+++ SpeakBTest/Features/Signup/Views/SignupView.swift
@@ -8,8 +8,6 @@
 import SwiftUI
 
 struct SignupView: View {
-    @State private var animate = false
-    
     var barData: [BarModel] = [
         BarModel(initialHeight: 0, finalHeight: 66, label: "現在"),
         BarModel(initialHeight: 0, finalHeight: 100, label: "3ヶ月"),
@@ -17,12 +15,29 @@ struct SignupView: View {
         BarModel(initialHeight: 0, finalHeight: 300, label: "2年")
     ]
     
+    private var linearBg: some View {
+        LinearGradient(gradient: Gradient(colors: [Color.white, Color.purpleBg]),
+                       startPoint: .bottom,
+                       endPoint: .top)
+        .ignoresSafeArea()
+    }
+    
+    private var signupBtn: some View {
+        Button(action: {
+            print("Button tapped!")
+        }) {
+            Text("プランに登録する")
+                .foregroundColor(.white)
+                .frame(width: 350, height: 50)
+                .background(Color.blue)
+                .clipShape(RoundedRectangle(cornerRadius: 25))
+        }
+    }
+    
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [Color.white, Color.purpleBg]),
-                           startPoint: .bottom,
-                           endPoint: .top)
-            .ignoresSafeArea()
+            linearBg
+            
             ScrollView {
                 VStack {
                     HStack {
@@ -51,40 +66,42 @@ struct SignupView: View {
                         .scaledToFit()
                         .frame(width: 187, height: 160)
                     
-                    HStack(alignment: .bottom, spacing: 16) {
-                        ForEach(0..<barData.count, id: \.self) { index in
-                            VStack {
-                                RoundedRectangle(cornerRadius: 4)
-                                    .fill(LinearGradient(gradient: Gradient(colors: [Color.gradientBottomBlue, Color.gradientTopBlue]), startPoint: .bottom, endPoint: .top))
-                                    .frame(width: 48, height: animate ? barData[index].finalHeight : barData[index].initialHeight, alignment: .bottom)
-                                    .animation(.easeInOut(duration: 1.0).delay(Double(index) * 0.2), value: animate)
-                                Text(barData[index].label)
-                                    .font(.system(size: 12, weight: .bold))
-                            }
-                        }
-                    }
-                    .frame(height: 300, alignment: .bottom)
-                    .padding()
-                    .onAppear() {
-                        animate = true
-                    }
+                    BarGraphView(barData: barData)
                     
                     Text("スピークバディで")
+                    
                     LinearGradientText("レベルアップ")
                         .padding(.top, 1)
                     
-                    Button(action: {
-                        print("Button tapped!")
-                    }) {
-                        Text("プランに登録する")
-                            .foregroundColor(.white)
-                            .frame(width: 350, height: 50)
-                            .background(Color.blue)
-                            .clipShape(RoundedRectangle(cornerRadius: 25))
-                    }
-                    .padding(.top, 16)
+                    signupBtn
+                        .padding(.top, 16)
                 }
             }
+        }
+    }
+}
+
+/// Creating a separate subview to increase readability
+private struct BarGraphView: View {
+    @State private var animate = false
+    let barData: [BarModel]
+    var body: some View {
+        HStack(alignment: .bottom, spacing: 16) {
+            ForEach(0..<barData.count, id: \.self) { index in
+                VStack {
+                    RoundedRectangle(cornerRadius: 4)
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color.gradientBottomBlue, Color.gradientTopBlue]), startPoint: .bottom, endPoint: .top))
+                        .frame(width: 48, height: animate ? barData[index].finalHeight : barData[index].initialHeight, alignment: .bottom)
+                        .animation(.easeInOut(duration: 1.0).delay(Double(index) * 0.2), value: animate)
+                    Text(barData[index].label)
+                        .font(.system(size: 12, weight: .bold))
+                }
+            }
+        }
+        .frame(height: 300, alignment: .bottom)
+        .padding()
+        .onAppear() {
+            animate = true
         }
     }
 }
